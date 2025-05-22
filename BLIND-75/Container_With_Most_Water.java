fb85fcf9c99300647d3f0d8de4598d731f3227c0
ri20045 <116284592+ri20045@users.noreply.github.com>
2025-05-22 17:33:20 +0530

    Create Container_With_Most_Water.java

---
+++ BLIND-75/Container_With_Most_Water.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int res = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            res = Math.max(res, area);
+
+            if (height[left] < height[right]) left ++;
+            else {
+                right--;
+            }
+        }
+        return res;
+    }
+}
