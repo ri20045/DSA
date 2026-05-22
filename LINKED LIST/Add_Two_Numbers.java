a5c0846be62c2183fb149e981a009f20230b9977
ri20045 <116284592+ri20045@users.noreply.github.com>
2026-05-22 21:21:50 +0530

    Add method to sum two numbers represented by linked lists
    Implement the addTwoNumbers method to sum two linked lists representing numbers.

---
+++ LINKED LIST/Add_Two_Numbers.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int v1 = 0;
+            if (l1 != null) {
+                v1 = l1.val;
+            }
+            int v2 = 0;
+            if (l2 != null) {
+                v2 = l2.val;
+            }
+
+            int sum = v1 + v2 + carry;
+            carry = sum/10;
+            int digit = sum % 10;
+            curr.next = new ListNode(digit);
+
+            curr = curr.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            } 
+        }
+        return dummy.next;
+    }
+}
